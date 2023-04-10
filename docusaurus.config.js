8d7ce89fc4ce91f9dd8b77896cc9babf18a8f3dd
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-04-10 09:50:33 -0400

    chore: update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,20 +6,21 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'CS 220',
+  tagline: 'Programming Methodology',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://umass-compsci-220.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'umass-compsci-220', // Usually your GitHub org/user name.
+  projectName: 'umass-compsci-220.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
