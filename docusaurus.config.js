ef4f0fadeb0aac664dd15d1a6f50fec5f58dda93
Daniel Melanson <30221758+daniel-melanson@users.noreply.github.com>
2023-04-13 13:52:33 -0400

    feat: homework

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,51 +1,55 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const math = require("remark-math");
+const katex = require("rehype-katex");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'CS 220',
-  tagline: 'Programming Methodology',
-  favicon: 'img/favicon.ico',
+  title: "CS 220",
+  tagline: "Programming Methodology",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://umass-compsci-220.github.io/',
+  url: "https://umass-compsci-220.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'umass-compsci-220', // Usually your GitHub org/user name.
-  projectName: 'umass-compsci-220.github.io', // Usually your repo name.
-  deploymentBranch: 'gh-pages',
+  organizationName: "umass-compsci-220", // Usually your GitHub org/user name.
+  projectName: "umass-compsci-220.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          path: './materials',
-          routeBasePath: 'materials',
-          sidebarPath: require.resolve('./sidebars.js'),
+          path: "./materials",
+          routeBasePath: "materials",
+          sidebarPath: require.resolve("./sidebars.js"),
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
         blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -55,31 +59,46 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'COMPSCI 220',
+        title: "COMPSCI 220",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "My Site Logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Materials',
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Materials",
+          },
+          {
+            to: "materials/logistics/syllabus",
+            label: "Syllabus",
+            position: "left",
+          },
+          {
+            to: "materials/logistics/schedule",
+            label: "Schedule",
+            position: "left",
+          },
+          {
+            to: "materials/resources/office-hours",
+            label: "Office Hours",
+            position: "left",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Tutorial",
+                to: "/docs/intro",
               },
             ],
           },
@@ -91,6 +110,14 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css",
+      type: "text/css",
+      integrity: "sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM",
+      crossorigin: "anonymous",
+    },
+  ],
 };
 
 module.exports = config;
